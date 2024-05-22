86d8a18c1a53c38c5c366de5dcd35538e26ec611
91Andrade08 <102134857+91Andrade08@users.noreply.github.com>
2024-05-22 19:59:26 +0000

    🚧 Entrega de Listas

--- routes/shop.routes.js
+++ routes/shop.routes.js
@@ -1,20 +1,18 @@
 // Importando el enrutador de express
 import { Router } from 'express';
-// Importando el gestor de rutas
-import path from 'path';
-
+// Importando el arreglo de productos
+import { products } from './admin.route.js';
 // Creando una instancia del enrutador de express
 const router = Router();
 
 // Importando productos
 import { products } from './admin.routes.js';
 
-// GET /
-router.get('/', (req, res)=>{
-  // Mostrando productos en memoria
-  console.log(products);
-  console.log("📢 Sirviendo la ruta '/'");
-  res.render('shop', {shop: 'active', docTitle:"Shop"});
+// La ruta raíz entra en todo tipo de petición
+router.get(["/", "/home"], (_, res) => {
+  console.log(`📔 Inventario de productos: ${JSON.stringify(products)}`);
+  console.log("📒 Sirviendo recurso: 'shop.html'");
+  res.render('shop', {shop: 'active', docTitle:"Tienda", products});
 });
 
 // GET /about
